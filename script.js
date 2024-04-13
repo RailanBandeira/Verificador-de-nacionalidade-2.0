e00249cffaba648c5eb89481ab1b2bfbd5bed264
Railan Andrade Bandeira <162853790+RailanBandeira@users.noreply.github.com>
2024-04-13 17:06:07 -0300

    Sintaxe mais semântica,Adicionado mais retornos para países

--- script.js
+++ script.js
@@ -9,14 +9,24 @@ function Verificar() {
     }
     else {
         if (vel == 'Brasil') {
-            ppp.innerHTML = `<p>Seu país é <strong>${vel}</strong></p>`
-
-            ppp.innerHTML += `<p>Você é <strong>brasileiro (a)</strong></p>`
+            ppp.innerHTML = `<p>Seu país é <strong>${vel}</strong></p>`;
+            ppp.innerHTML += `<p>Você é <strong>brasileiro (a)</strong></p>`;
+        }
+        else if (vel == 'Estados Unidos' || vel == 'USA') {
+            ppp.innerHTML = `<p>Seu país é <strong>${vel}</strong></p>`;
+            ppp.innerHTML += `<p>Você é <strong>americano(a)</strong></p>`;
+        }
+        else if (vel == 'Russia') {
+            ppp.innerHTML = `<p>Seu país é <strong>${vel}</strong></p>`;
+            ppp.innerHTML += `<p>Você é <strong>russo(a)</strong></p>`;
+        }
+        else if (vel == 'China') {
+            ppp.innerHTML = `<p>Seu país é <strong>${vel}</strong></p>`;
+            ppp.innerHTML += `<p>Você é <strong>chinês(a)</strong></p>`;
         }
         else {
-            ppp.innerHTML = `<p>Seu país é <strong>${vel}</strong></p>`
-
-            ppp.innerHTML += `<p>Você é <strong>estrangeiro (a)</strong></p>`
+            ppp.innerHTML = `<p>Seu país é <strong>${vel}</strong></p>`;
+            ppp.innerHTML += `<p>Você é <strong>estrangeiro (a)</strong></p>`;
         }
     }
 }
